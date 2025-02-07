ce718d5e3af0270f7dd06660d6c7a1d7da98f743
Hung-Yi Loo <hungyi.loo@gmail.com>
2025-02-07 19:18:38 +0800

    feat: add yazi setup

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -47,4 +47,15 @@ function Reset-Path
 
 # PSFzf setup
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+
+# Yazi setup
+$env:YAZI_FILE_ONE = "C:\Program Files\Git\usr\bin\file.exe"
+function y {
+  $tmp = [System.IO.Path]::GetTempFileName()
+  yazi $args --cwd-file="$tmp"
+  $cwd = Get-Content -Path $tmp -Encoding UTF8
+  if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+    Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+  }
+  Remove-Item -Path $tmp
+}
